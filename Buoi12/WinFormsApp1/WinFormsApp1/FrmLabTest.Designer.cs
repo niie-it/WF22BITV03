62d156ff8d57a582f02f9be3e1a6ed9378f812d9
Hien Luong Tran Hy <hyhien@gmail.com>
2025-03-21 23:23:59 +0700

    Mid-test 22bitv01

---
+++ Buoi12/WinFormsApp1/WinFormsApp1/FrmLabTest.Designer.cs
@@ -0,0 +1,350 @@
+﻿namespace WinFormsApp1
+{
+    partial class FrmLabTest
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            label1 = new Label();
+            groupBox1 = new GroupBox();
+            radioButton1 = new RadioButton();
+            radioButton2 = new RadioButton();
+            groupBox2 = new GroupBox();
+            label2 = new Label();
+            label3 = new Label();
+            textBox1 = new TextBox();
+            textBox2 = new TextBox();
+            groupBox3 = new GroupBox();
+            textBox3 = new TextBox();
+            textBox4 = new TextBox();
+            label4 = new Label();
+            label5 = new Label();
+            button1 = new Button();
+            button2 = new Button();
+            button3 = new Button();
+            button4 = new Button();
+            textBox5 = new TextBox();
+            textBox6 = new TextBox();
+            textBox7 = new TextBox();
+            label6 = new Label();
+            label7 = new Label();
+            groupBox1.SuspendLayout();
+            groupBox2.SuspendLayout();
+            groupBox3.SuspendLayout();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Tahoma", 21.9F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label1.ForeColor = Color.Blue;
+            label1.Location = new Point(23, 30);
+            label1.Name = "label1";
+            label1.Size = new Size(1585, 88);
+            label1.TabIndex = 0;
+            label1.Text = "CHƯƠNG TRÌNH TÍNH LƯƠNG NHÂN VIÊN";
+            // 
+            // groupBox1
+            // 
+            groupBox1.Controls.Add(radioButton2);
+            groupBox1.Controls.Add(radioButton1);
+            groupBox1.Font = new Font("Times New Roman", 14.1F);
+            groupBox1.Location = new Point(49, 159);
+            groupBox1.Name = "groupBox1";
+            groupBox1.Size = new Size(694, 234);
+            groupBox1.TabIndex = 1;
+            groupBox1.TabStop = false;
+            groupBox1.Text = "Loại nhân viên";
+            // 
+            // radioButton1
+            // 
+            radioButton1.AutoSize = true;
+            radioButton1.Checked = true;
+            radioButton1.Font = new Font("Times New Roman", 14.1F);
+            radioButton1.Location = new Point(44, 104);
+            radioButton1.Name = "radioButton1";
+            radioButton1.Size = new Size(223, 57);
+            radioButton1.TabIndex = 0;
+            radioButton1.TabStop = true;
+            radioButton1.Text = "Sản xuất";
+            radioButton1.UseVisualStyleBackColor = true;
+            // 
+            // radioButton2
+            // 
+            radioButton2.AutoSize = true;
+            radioButton2.Font = new Font("Times New Roman", 14.1F);
+            radioButton2.Location = new Point(358, 102);
+            radioButton2.Name = "radioButton2";
+            radioButton2.Size = new Size(193, 57);
+            radioButton2.TabIndex = 1;
+            radioButton2.Text = "Tạp vụ";
+            radioButton2.UseVisualStyleBackColor = true;
+            // 
+            // groupBox2
+            // 
+            groupBox2.Controls.Add(textBox2);
+            groupBox2.Controls.Add(textBox1);
+            groupBox2.Controls.Add(label3);
+            groupBox2.Controls.Add(label2);
+            groupBox2.Font = new Font("Times New Roman", 14.1F);
+            groupBox2.Location = new Point(50, 421);
+            groupBox2.Name = "groupBox2";
+            groupBox2.Size = new Size(693, 250);
+            groupBox2.TabIndex = 2;
+            groupBox2.TabStop = false;
+            groupBox2.Text = "Nhân viên Tạp vụ";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Times New Roman", 14.1F);
+            label2.Location = new Point(28, 81);
+            label2.Name = "label2";
+            label2.Size = new Size(285, 53);
+            label2.TabIndex = 0;
+            label2.Text = "Lương cơ bản";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Font = new Font("Times New Roman", 14.1F);
+            label3.Location = new Point(28, 166);
+            label3.Name = "label3";
+            label3.Size = new Size(174, 53);
+            label3.TabIndex = 0;
+            label3.Text = "Phụ cấp";
+            // 
+            // textBox1
+            // 
+            textBox1.Font = new Font("Times New Roman", 14.1F);
+            textBox1.Location = new Point(346, 78);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(241, 62);
+            textBox1.TabIndex = 1;
+            // 
+            // textBox2
+            // 
+            textBox2.Font = new Font("Times New Roman", 14.1F);
+            textBox2.Location = new Point(346, 163);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(250, 62);
+            textBox2.TabIndex = 2;
+            // 
+            // groupBox3
+            // 
+            groupBox3.Controls.Add(textBox3);
+            groupBox3.Controls.Add(textBox4);
+            groupBox3.Controls.Add(label4);
+            groupBox3.Controls.Add(label5);
+            groupBox3.Font = new Font("Times New Roman", 14.1F);
+            groupBox3.Location = new Point(900, 437);
+            groupBox3.Name = "groupBox3";
+            groupBox3.Size = new Size(693, 250);
+            groupBox3.TabIndex = 2;
+            groupBox3.TabStop = false;
+            groupBox3.Text = "Nhân viên Sản xuất";
+            // 
+            // textBox3
+            // 
+            textBox3.Font = new Font("Times New Roman", 14.1F);
+            textBox3.Location = new Point(308, 157);
+            textBox3.Name = "textBox3";
+            textBox3.Size = new Size(250, 62);
+            textBox3.TabIndex = 2;
+            // 
+            // textBox4
+            // 
+            textBox4.Font = new Font("Times New Roman", 14.1F);
+            textBox4.Location = new Point(308, 72);
+            textBox4.Name = "textBox4";
+            textBox4.Size = new Size(241, 62);
+            textBox4.TabIndex = 1;
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Font = new Font("Times New Roman", 14.1F);
+            label4.Location = new Point(28, 166);
+            label4.Name = "label4";
+            label4.Size = new Size(174, 53);
+            label4.TabIndex = 0;
+            label4.Text = "Phụ cấp";
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Font = new Font("Times New Roman", 14.1F);
+            label5.Location = new Point(28, 81);
+            label5.Name = "label5";
+            label5.Size = new Size(264, 53);
+            label5.TabIndex = 0;
+            label5.Text = "Số sản phẩm";
+            // 
+            // button1
+            // 
+            button1.Font = new Font("Times New Roman", 14.1F);
+            button1.Location = new Point(59, 693);
+            button1.Name = "button1";
+            button1.Size = new Size(263, 105);
+            button1.TabIndex = 3;
+            button1.Text = "Thêm";
+            button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            button2.Font = new Font("Times New Roman", 14.1F);
+            button2.Location = new Point(420, 693);
+            button2.Name = "button2";
+            button2.Size = new Size(263, 105);
+            button2.TabIndex = 3;
+            button2.Text = "Nhập lại";
+            button2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            button3.Font = new Font("Times New Roman", 14.1F);
+            button3.Location = new Point(807, 693);
+            button3.Name = "button3";
+            button3.Size = new Size(263, 105);
+            button3.TabIndex = 3;
+            button3.Text = "Lưu file";
+            button3.UseVisualStyleBackColor = true;
+            // 
+            // button4
+            // 
+            button4.Font = new Font("Times New Roman", 14.1F);
+            button4.Location = new Point(1186, 693);
+            button4.Name = "button4";
+            button4.Size = new Size(263, 105);
+            button4.TabIndex = 3;
+            button4.Text = "Thoát";
+            button4.UseVisualStyleBackColor = true;
+            // 
+            // textBox5
+            // 
+            textBox5.Font = new Font("Times New Roman", 14.1F);
+            textBox5.Location = new Point(50, 827);
+            textBox5.Multiline = true;
+            textBox5.Name = "textBox5";
+            textBox5.Size = new Size(1520, 277);
+            textBox5.TabIndex = 4;
+            textBox5.Text = "NV01, Lê Li La, NV Sản xuất, Số SP: 10, Phụ cấp: 10000, Lương 1,100,000 đ";
+            // 
+            // textBox6
+            // 
+            textBox6.Font = new Font("Times New Roman", 14.1F);
+            textBox6.Location = new Point(1180, 285);
+            textBox6.Name = "textBox6";
+            textBox6.Size = new Size(374, 62);
+            textBox6.TabIndex = 8;
+            // 
+            // textBox7
+            // 
+            textBox7.Font = new Font("Times New Roman", 14.1F);
+            textBox7.Location = new Point(1180, 200);
+            textBox7.Name = "textBox7";
+            textBox7.Size = new Size(374, 62);
+            textBox7.TabIndex = 7;
+            // 
+            // label6
+            // 
+            label6.AutoSize = true;
+            label6.Font = new Font("Times New Roman", 14.1F);
+            label6.Location = new Point(900, 294);
+            label6.Name = "label6";
+            label6.Size = new Size(149, 53);
+            label6.TabIndex = 5;
+            label6.Text = "Họ tên";
+            // 
+            // label7
+            // 
+            label7.AutoSize = true;
+            label7.Font = new Font("Times New Roman", 14.1F);
+            label7.Location = new Point(900, 209);
+            label7.Name = "label7";
+            label7.Size = new Size(165, 53);
+            label7.TabIndex = 6;
+            label7.Text = "Mã NV";
+            // 
+            // FrmLabTest
+            // 
+            AutoScaleDimensions = new SizeF(17F, 41F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(1642, 1132);
+            Controls.Add(textBox6);
+            Controls.Add(textBox7);
+            Controls.Add(label6);
+            Controls.Add(label7);
+            Controls.Add(textBox5);
+            Controls.Add(button4);
+            Controls.Add(button3);
+            Controls.Add(button2);
+            Controls.Add(button1);
+            Controls.Add(groupBox3);
+            Controls.Add(groupBox2);
+            Controls.Add(groupBox1);
+            Controls.Add(label1);
+            MaximizeBox = false;
+            MinimizeBox = false;
+            Name = "FrmLabTest";
+            Text = "2202120120  Lê Li La";
+            groupBox1.ResumeLayout(false);
+            groupBox1.PerformLayout();
+            groupBox2.ResumeLayout(false);
+            groupBox2.PerformLayout();
+            groupBox3.ResumeLayout(false);
+            groupBox3.PerformLayout();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label label1;
+        private GroupBox groupBox1;
+        private RadioButton radioButton2;
+        private RadioButton radioButton1;
+        private GroupBox groupBox2;
+        private TextBox textBox2;
+        private TextBox textBox1;
+        private Label label3;
+        private Label label2;
+        private GroupBox groupBox3;
+        private TextBox textBox3;
+        private TextBox textBox4;
+        private Label label4;
+        private Label label5;
+        private Button button1;
+        private Button button2;
+        private Button button3;
+        private Button button4;
+        private TextBox textBox5;
+        private TextBox textBox6;
+        private TextBox textBox7;
+        private Label label6;
+        private Label label7;
+    }
+}
