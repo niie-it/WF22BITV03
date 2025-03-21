094e23de271dd6654f89575c06c79adc0b7095c0
Hien Luong Tran Hy <hyhien@gmail.com>
2025-03-21 23:41:57 +0700

    De 22BITV03

---
+++ Buoi12/WinFormsApp1/WinFormsApp1/FrmTest22BITV03.Designer.cs
@@ -0,0 +1,355 @@
+﻿namespace WinFormsApp1
+{
+    partial class FrmTest22BITV03
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            txtKetQua = new TextBox();
+            button4 = new Button();
+            button3 = new Button();
+            button2 = new Button();
+            button1 = new Button();
+            textBox3 = new TextBox();
+            textBox4 = new TextBox();
+            label2 = new Label();
+            label4 = new Label();
+            label5 = new Label();
+            textBox2 = new TextBox();
+            textBox1 = new TextBox();
+            label3 = new Label();
+            groupBox2 = new GroupBox();
+            radioButton2 = new RadioButton();
+            radioButton1 = new RadioButton();
+            groupBox3 = new GroupBox();
+            groupBox1 = new GroupBox();
+            label1 = new Label();
+            radioButton3 = new RadioButton();
+            groupBox4 = new GroupBox();
+            textBox7 = new TextBox();
+            label7 = new Label();
+            groupBox2.SuspendLayout();
+            groupBox3.SuspendLayout();
+            groupBox1.SuspendLayout();
+            groupBox4.SuspendLayout();
+            SuspendLayout();
+            // 
+            // txtKetQua
+            // 
+            txtKetQua.Font = new Font("Times New Roman", 14.1F);
+            txtKetQua.Location = new Point(49, 822);
+            txtKetQua.Multiline = true;
+            txtKetQua.Name = "txtKetQua";
+            txtKetQua.Size = new Size(1520, 378);
+            txtKetQua.TabIndex = 17;
+            // 
+            // button4
+            // 
+            button4.Font = new Font("Times New Roman", 14.1F);
+            button4.Location = new Point(1185, 688);
+            button4.Name = "button4";
+            button4.Size = new Size(263, 105);
+            button4.TabIndex = 13;
+            button4.Text = "Thoát";
+            button4.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            button3.Font = new Font("Times New Roman", 14.1F);
+            button3.Location = new Point(806, 688);
+            button3.Name = "button3";
+            button3.Size = new Size(263, 105);
+            button3.TabIndex = 14;
+            button3.Text = "Lưu file";
+            button3.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            button2.Font = new Font("Times New Roman", 14.1F);
+            button2.Location = new Point(419, 688);
+            button2.Name = "button2";
+            button2.Size = new Size(263, 105);
+            button2.TabIndex = 15;
+            button2.Text = "Nhập lại";
+            button2.UseVisualStyleBackColor = true;
+            // 
+            // button1
+            // 
+            button1.Font = new Font("Times New Roman", 14.1F);
+            button1.Location = new Point(58, 688);
+            button1.Name = "button1";
+            button1.Size = new Size(263, 105);
+            button1.TabIndex = 16;
+            button1.Text = "Thêm";
+            button1.UseVisualStyleBackColor = true;
+            // 
+            // textBox3
+            // 
+            textBox3.Font = new Font("Times New Roman", 14.1F);
+            textBox3.Location = new Point(270, 157);
+            textBox3.Name = "textBox3";
+            textBox3.Size = new Size(241, 62);
+            textBox3.TabIndex = 2;
+            // 
+            // textBox4
+            // 
+            textBox4.Font = new Font("Times New Roman", 14.1F);
+            textBox4.Location = new Point(270, 72);
+            textBox4.Name = "textBox4";
+            textBox4.Size = new Size(241, 62);
+            textBox4.TabIndex = 1;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Times New Roman", 14.1F);
+            label2.Location = new Point(28, 81);
+            label2.Name = "label2";
+            label2.Size = new Size(203, 53);
+            label2.TabIndex = 0;
+            label2.Text = "Chiều dài";
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Font = new Font("Times New Roman", 14.1F);
+            label4.Location = new Point(28, 166);
+            label4.Name = "label4";
+            label4.Size = new Size(213, 53);
+            label4.TabIndex = 0;
+            label4.Text = "Trục ngắn";
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Font = new Font("Times New Roman", 14.1F);
+            label5.Location = new Point(28, 81);
+            label5.Name = "label5";
+            label5.Size = new Size(180, 53);
+            label5.TabIndex = 0;
+            label5.Text = "Trục dài";
+            // 
+            // textBox2
+            // 
+            textBox2.Font = new Font("Times New Roman", 14.1F);
+            textBox2.Location = new Point(285, 163);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(241, 62);
+            textBox2.TabIndex = 2;
+            // 
+            // textBox1
+            // 
+            textBox1.Font = new Font("Times New Roman", 14.1F);
+            textBox1.Location = new Point(285, 78);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(241, 62);
+            textBox1.TabIndex = 1;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Font = new Font("Times New Roman", 14.1F);
+            label3.Location = new Point(28, 166);
+            label3.Name = "label3";
+            label3.Size = new Size(232, 53);
+            label3.TabIndex = 0;
+            label3.Text = "Chiều rộng";
+            // 
+            // groupBox2
+            // 
+            groupBox2.Controls.Add(textBox2);
+            groupBox2.Controls.Add(textBox1);
+            groupBox2.Controls.Add(label3);
+            groupBox2.Controls.Add(label2);
+            groupBox2.Font = new Font("Times New Roman", 14.1F);
+            groupBox2.Location = new Point(49, 416);
+            groupBox2.Name = "groupBox2";
+            groupBox2.Size = new Size(560, 250);
+            groupBox2.TabIndex = 11;
+            groupBox2.TabStop = false;
+            groupBox2.Text = "Hình chữ nhật";
+            // 
+            // radioButton2
+            // 
+            radioButton2.AutoSize = true;
+            radioButton2.Font = new Font("Times New Roman", 14.1F);
+            radioButton2.Location = new Point(358, 102);
+            radioButton2.Name = "radioButton2";
+            radioButton2.Size = new Size(150, 57);
+            radioButton2.TabIndex = 1;
+            radioButton2.Text = "Tròn";
+            radioButton2.UseVisualStyleBackColor = true;
+            // 
+            // radioButton1
+            // 
+            radioButton1.AutoSize = true;
+            radioButton1.Checked = true;
+            radioButton1.Font = new Font("Times New Roman", 14.1F);
+            radioButton1.Location = new Point(44, 104);
+            radioButton1.Name = "radioButton1";
+            radioButton1.Size = new Size(231, 57);
+            radioButton1.TabIndex = 0;
+            radioButton1.TabStop = true;
+            radioButton1.Text = "Chữ nhật";
+            radioButton1.UseVisualStyleBackColor = true;
+            // 
+            // groupBox3
+            // 
+            groupBox3.Controls.Add(textBox3);
+            groupBox3.Controls.Add(textBox4);
+            groupBox3.Controls.Add(label4);
+            groupBox3.Controls.Add(label5);
+            groupBox3.Font = new Font("Times New Roman", 14.1F);
+            groupBox3.Location = new Point(1008, 432);
+            groupBox3.Name = "groupBox3";
+            groupBox3.Size = new Size(540, 250);
+            groupBox3.TabIndex = 12;
+            groupBox3.TabStop = false;
+            groupBox3.Text = "Hình ellipse";
+            // 
+            // groupBox1
+            // 
+            groupBox1.Controls.Add(radioButton3);
+            groupBox1.Controls.Add(radioButton2);
+            groupBox1.Controls.Add(radioButton1);
+            groupBox1.Font = new Font("Times New Roman", 14.1F);
+            groupBox1.Location = new Point(48, 154);
+            groupBox1.Name = "groupBox1";
+            groupBox1.Size = new Size(898, 234);
+            groupBox1.TabIndex = 10;
+            groupBox1.TabStop = false;
+            groupBox1.Text = "Loại hình";
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Tahoma", 21.9F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label1.ForeColor = Color.Blue;
+            label1.Location = new Point(22, 25);
+            label1.Name = "label1";
+            label1.Size = new Size(1391, 88);
+            label1.TabIndex = 9;
+            label1.Text = "CHƯƠNG TRÌNH QUẢN LÝ HÌNH HỌC";
+            label1.Click += label1_Click;
+            // 
+            // radioButton3
+            // 
+            radioButton3.AutoSize = true;
+            radioButton3.Font = new Font("Times New Roman", 14.1F);
+            radioButton3.Location = new Point(625, 102);
+            radioButton3.Name = "radioButton3";
+            radioButton3.Size = new Size(188, 57);
+            radioButton3.TabIndex = 1;
+            radioButton3.Text = "Ellipse";
+            radioButton3.UseVisualStyleBackColor = true;
+            // 
+            // groupBox4
+            // 
+            groupBox4.Controls.Add(textBox7);
+            groupBox4.Controls.Add(label7);
+            groupBox4.Font = new Font("Times New Roman", 14.1F);
+            groupBox4.Location = new Point(1008, 176);
+            groupBox4.Name = "groupBox4";
+            groupBox4.Size = new Size(540, 212);
+            groupBox4.TabIndex = 12;
+            groupBox4.TabStop = false;
+            groupBox4.Text = "Hình tròn";
+            // 
+            // textBox7
+            // 
+            textBox7.Font = new Font("Times New Roman", 14.1F);
+            textBox7.Location = new Point(272, 109);
+            textBox7.Name = "textBox7";
+            textBox7.Size = new Size(241, 62);
+            textBox7.TabIndex = 1;
+            // 
+            // label7
+            // 
+            label7.AutoSize = true;
+            label7.Font = new Font("Times New Roman", 14.1F);
+            label7.Location = new Point(30, 118);
+            label7.Name = "label7";
+            label7.Size = new Size(192, 53);
+            label7.TabIndex = 0;
+            label7.Text = "Bán kính";
+            // 
+            // FrmTest22BITV03
+            // 
+            AutoScaleDimensions = new SizeF(17F, 41F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(1618, 1234);
+            ControlBox = false;
+            Controls.Add(txtKetQua);
+            Controls.Add(button4);
+            Controls.Add(button3);
+            Controls.Add(button2);
+            Controls.Add(button1);
+            Controls.Add(groupBox2);
+            Controls.Add(groupBox4);
+            Controls.Add(groupBox3);
+            Controls.Add(groupBox1);
+            Controls.Add(label1);
+            Name = "FrmTest22BITV03";
+            Text = "220284312 Lê Li a";
+            Load += FrmTest22BITV03_Load;
+            groupBox2.ResumeLayout(false);
+            groupBox2.PerformLayout();
+            groupBox3.ResumeLayout(false);
+            groupBox3.PerformLayout();
+            groupBox1.ResumeLayout(false);
+            groupBox1.PerformLayout();
+            groupBox4.ResumeLayout(false);
+            groupBox4.PerformLayout();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+        private TextBox txtKetQua;
+        private Button button4;
+        private Button button3;
+        private Button button2;
+        private Button button1;
+        private TextBox textBox3;
+        private TextBox textBox4;
+        private Label label2;
+        private Label label4;
+        private Label label5;
+        private TextBox textBox2;
+        private TextBox textBox1;
+        private Label label3;
+        private GroupBox groupBox2;
+        private RadioButton radioButton2;
+        private RadioButton radioButton1;
+        private GroupBox groupBox3;
+        private GroupBox groupBox1;
+        private Label label1;
+        private RadioButton radioButton3;
+        private GroupBox groupBox4;
+        private TextBox textBox7;
+        private Label label7;
+    }
+}
---
+++ Buoi12/WinFormsApp1/WinFormsApp1/FrmTest22BITV03.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WinFormsApp1
+{
+    public partial class FrmTest22BITV03 : Form
+    {
+        public FrmTest22BITV03()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void FrmTest22BITV03_Load(object sender, EventArgs e)
+        {
+            txtKetQua.Text = "Tròn, R = 2.1, S = 13.847\r\n";
+            txtKetQua.AppendText("Chữ nhật, D = 8, R = 6, S = 48\r\n");
+            txtKetQua.AppendText("Ellipse, a = 8, b = 6, S = 37.68");
+        }
+    }
+}
