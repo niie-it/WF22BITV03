6d32273731d542164d0cf18288c841e814afd73d
Hien Luong Tran Hy <hyhien@gmail.com>
2025-03-24 14:20:24 +0700

    Mid Test 3

---
+++ Buoi12/WinFormsApp1/WinFormsApp1/MidTermTest.Designer.cs
@@ -0,0 +1,443 @@
+﻿namespace WinFormsApp1
+{
+    partial class MidTermTest
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MidTermTest));
+            textBox6 = new TextBox();
+            textBox7 = new TextBox();
+            label6 = new Label();
+            label7 = new Label();
+            textBox5 = new TextBox();
+            button4 = new Button();
+            button3 = new Button();
+            button2 = new Button();
+            button1 = new Button();
+            label2 = new Label();
+            textBox2 = new TextBox();
+            textBox1 = new TextBox();
+            label3 = new Label();
+            groupBox2 = new GroupBox();
+            label1 = new Label();
+            label8 = new Label();
+            comboBox1 = new ComboBox();
+            label9 = new Label();
+            textBox8 = new TextBox();
+            label4 = new Label();
+            textBox3 = new TextBox();
+            label5 = new Label();
+            textBox4 = new TextBox();
+            groupBox1 = new GroupBox();
+            textBox9 = new TextBox();
+            textBox10 = new TextBox();
+            textBox11 = new TextBox();
+            label10 = new Label();
+            label11 = new Label();
+            label12 = new Label();
+            groupBox2.SuspendLayout();
+            groupBox1.SuspendLayout();
+            SuspendLayout();
+            // 
+            // textBox6
+            // 
+            textBox6.Font = new Font("Times New Roman", 14.1F);
+            textBox6.Location = new Point(267, 297);
+            textBox6.Name = "textBox6";
+            textBox6.Size = new Size(1792, 62);
+            textBox6.TabIndex = 21;
+            // 
+            // textBox7
+            // 
+            textBox7.Font = new Font("Times New Roman", 14.1F);
+            textBox7.Location = new Point(267, 184);
+            textBox7.Name = "textBox7";
+            textBox7.Size = new Size(700, 62);
+            textBox7.TabIndex = 20;
+            // 
+            // label6
+            // 
+            label6.AutoSize = true;
+            label6.Font = new Font("Times New Roman", 14.1F);
+            label6.Location = new Point(34, 300);
+            label6.Name = "label6";
+            label6.Size = new Size(149, 53);
+            label6.TabIndex = 18;
+            label6.Text = "Họ tên";
+            // 
+            // label7
+            // 
+            label7.AutoSize = true;
+            label7.Font = new Font("Times New Roman", 14.1F);
+            label7.Location = new Point(1112, 190);
+            label7.Name = "label7";
+            label7.Size = new Size(225, 53);
+            label7.TabIndex = 19;
+            label7.Text = "Tỉnh thành";
+            // 
+            // textBox5
+            // 
+            textBox5.Font = new Font("Times New Roman", 14.1F);
+            textBox5.Location = new Point(55, 1043);
+            textBox5.Multiline = true;
+            textBox5.Name = "textBox5";
+            textBox5.Size = new Size(2034, 595);
+            textBox5.TabIndex = 17;
+            textBox5.Text = resources.GetString("textBox5.Text");
+            // 
+            // button4
+            // 
+            button4.Font = new Font("Times New Roman", 14.1F);
+            button4.Location = new Point(1696, 527);
+            button4.Name = "button4";
+            button4.Size = new Size(380, 105);
+            button4.TabIndex = 13;
+            button4.Text = "Thoát";
+            button4.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            button3.Font = new Font("Times New Roman", 14.1F);
+            button3.Location = new Point(1696, 381);
+            button3.Name = "button3";
+            button3.Size = new Size(380, 105);
+            button3.TabIndex = 14;
+            button3.Text = "Lưu file";
+            button3.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            button2.Font = new Font("Times New Roman", 14.1F);
+            button2.Location = new Point(1269, 521);
+            button2.Name = "button2";
+            button2.Size = new Size(380, 105);
+            button2.TabIndex = 15;
+            button2.Text = "Nhập lại";
+            button2.UseVisualStyleBackColor = true;
+            // 
+            // button1
+            // 
+            button1.Font = new Font("Times New Roman", 14.1F);
+            button1.Location = new Point(1269, 384);
+            button1.Name = "button1";
+            button1.Size = new Size(380, 105);
+            button1.TabIndex = 16;
+            button1.Text = "Tính toán";
+            button1.UseVisualStyleBackColor = true;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Times New Roman", 14.1F);
+            label2.Location = new Point(42, 407);
+            label2.Name = "label2";
+            label2.Size = new Size(532, 53);
+            label2.TabIndex = 0;
+            label2.Text = "Lương hợp đồng (GROSS)";
+            // 
+            // textBox2
+            // 
+            textBox2.BackColor = SystemColors.Menu;
+            textBox2.Font = new Font("Times New Roman", 14.1F, FontStyle.Bold);
+            textBox2.ForeColor = Color.Blue;
+            textBox2.Location = new Point(581, 163);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(411, 62);
+            textBox2.TabIndex = 2;
+            textBox2.Text = "0";
+            // 
+            // textBox1
+            // 
+            textBox1.Font = new Font("Times New Roman", 14.1F);
+            textBox1.Location = new Point(623, 407);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(420, 62);
+            textBox1.TabIndex = 1;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Font = new Font("Times New Roman", 14.1F);
+            label3.Location = new Point(28, 166);
+            label3.Name = "label3";
+            label3.Size = new Size(438, 53);
+            label3.TabIndex = 0;
+            label3.Text = "Bảo hiểm xã hội (8%)";
+            // 
+            // groupBox2
+            // 
+            groupBox2.Controls.Add(textBox3);
+            groupBox2.Controls.Add(textBox4);
+            groupBox2.Controls.Add(textBox2);
+            groupBox2.Controls.Add(label5);
+            groupBox2.Controls.Add(label4);
+            groupBox2.Controls.Add(label3);
+            groupBox2.Font = new Font("Times New Roman", 14.1F);
+            groupBox2.Location = new Point(42, 648);
+            groupBox2.Name = "groupBox2";
+            groupBox2.Size = new Size(1016, 341);
+            groupBox2.TabIndex = 11;
+            groupBox2.TabStop = false;
+            groupBox2.Text = "Thông tin bảo hiểm";
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Tahoma", 21.9F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label1.ForeColor = Color.Blue;
+            label1.Location = new Point(338, 22);
+            label1.Name = "label1";
+            label1.Size = new Size(1399, 88);
+            label1.TabIndex = 9;
+            label1.Text = "THÔNG TIN TIỀN LƯƠNG NHÂN VIÊN";
+            // 
+            // label8
+            // 
+            label8.AutoSize = true;
+            label8.Font = new Font("Times New Roman", 14.1F);
+            label8.Location = new Point(34, 193);
+            label8.Name = "label8";
+            label8.Size = new Size(165, 53);
+            label8.TabIndex = 19;
+            label8.Text = "Mã NV";
+            // 
+            // comboBox1
+            // 
+            comboBox1.Font = new Font("Times New Roman", 14.1F);
+            comboBox1.FormattingEnabled = true;
+            comboBox1.Items.AddRange(new object[] { "Hà Nội", "Hồ Chí Minh", "Huế", "Đã Nẵng", "Cần Thơ", "Long An", "Hải phòng" });
+            comboBox1.Location = new Point(1359, 184);
+            comboBox1.Name = "comboBox1";
+            comboBox1.Size = new Size(700, 61);
+            comboBox1.TabIndex = 22;
+            // 
+            // label9
+            // 
+            label9.AutoSize = true;
+            label9.Font = new Font("Times New Roman", 14.1F);
+            label9.Location = new Point(42, 530);
+            label9.Name = "label9";
+            label9.Size = new Size(393, 53);
+            label9.TabIndex = 0;
+            label9.Text = "Số người phụ thuộc";
+            // 
+            // textBox8
+            // 
+            textBox8.Font = new Font("Times New Roman", 14.1F);
+            textBox8.Location = new Point(623, 521);
+            textBox8.Name = "textBox8";
+            textBox8.Size = new Size(411, 62);
+            textBox8.TabIndex = 2;
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Font = new Font("Times New Roman", 14.1F);
+            label4.Location = new Point(28, 69);
+            label4.Name = "label4";
+            label4.Size = new Size(426, 53);
+            label4.TabIndex = 0;
+            label4.Text = "Bảo hiểm y tế (1.5%)";
+            // 
+            // textBox3
+            // 
+            textBox3.BackColor = SystemColors.Menu;
+            textBox3.Font = new Font("Times New Roman", 14.1F, FontStyle.Bold);
+            textBox3.ForeColor = Color.Blue;
+            textBox3.Location = new Point(581, 66);
+            textBox3.Name = "textBox3";
+            textBox3.Size = new Size(411, 62);
+            textBox3.TabIndex = 2;
+            textBox3.Text = "0";
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Font = new Font("Times New Roman", 14.1F);
+            label5.Location = new Point(25, 258);
+            label5.Name = "label5";
+            label5.Size = new Size(531, 53);
+            label5.TabIndex = 0;
+            label5.Text = "Bảo hiểm thất nghiệp (1%)";
+            // 
+            // textBox4
+            // 
+            textBox4.BackColor = SystemColors.Menu;
+            textBox4.Font = new Font("Times New Roman", 14.1F, FontStyle.Bold);
+            textBox4.ForeColor = Color.Blue;
+            textBox4.Location = new Point(578, 255);
+            textBox4.Name = "textBox4";
+            textBox4.Size = new Size(411, 62);
+            textBox4.TabIndex = 2;
+            textBox4.Text = "0";
+            // 
+            // groupBox1
+            // 
+            groupBox1.Controls.Add(textBox9);
+            groupBox1.Controls.Add(textBox10);
+            groupBox1.Controls.Add(textBox11);
+            groupBox1.Controls.Add(label10);
+            groupBox1.Controls.Add(label11);
+            groupBox1.Controls.Add(label12);
+            groupBox1.Font = new Font("Times New Roman", 14.1F);
+            groupBox1.Location = new Point(1097, 664);
+            groupBox1.Name = "groupBox1";
+            groupBox1.Size = new Size(1016, 341);
+            groupBox1.TabIndex = 11;
+            groupBox1.TabStop = false;
+            groupBox1.Text = "Thông tin thuế TNCN";
+            // 
+            // textBox9
+            // 
+            textBox9.BackColor = SystemColors.Menu;
+            textBox9.Font = new Font("Times New Roman", 14.1F, FontStyle.Bold);
+            textBox9.ForeColor = Color.Blue;
+            textBox9.Location = new Point(581, 66);
+            textBox9.Name = "textBox9";
+            textBox9.Size = new Size(411, 62);
+            textBox9.TabIndex = 2;
+            textBox9.Text = "11,000,000";
+            // 
+            // textBox10
+            // 
+            textBox10.BackColor = SystemColors.Menu;
+            textBox10.Font = new Font("Times New Roman", 18F, FontStyle.Bold);
+            textBox10.ForeColor = Color.Red;
+            textBox10.Location = new Point(578, 255);
+            textBox10.Name = "textBox10";
+            textBox10.Size = new Size(411, 76);
+            textBox10.TabIndex = 2;
+            textBox10.Text = "0";
+            // 
+            // textBox11
+            // 
+            textBox11.BackColor = SystemColors.Menu;
+            textBox11.Font = new Font("Times New Roman", 14.1F, FontStyle.Bold);
+            textBox11.ForeColor = Color.Blue;
+            textBox11.Location = new Point(581, 163);
+            textBox11.Name = "textBox11";
+            textBox11.Size = new Size(411, 62);
+            textBox11.TabIndex = 2;
+            textBox11.Text = "0";
+            // 
+            // label10
+            // 
+            label10.AutoSize = true;
+            label10.Font = new Font("Times New Roman", 14.1F);
+            label10.Location = new Point(25, 258);
+            label10.Name = "label10";
+            label10.Size = new Size(223, 53);
+            label10.TabIndex = 0;
+            label10.Text = "Thực nhận";
+            // 
+            // label11
+            // 
+            label11.AutoSize = true;
+            label11.Font = new Font("Times New Roman", 14.1F);
+            label11.Location = new Point(28, 69);
+            label11.Name = "label11";
+            label11.Size = new Size(349, 53);
+            label11.TabIndex = 0;
+            label11.Text = "Giảm trừ cá nhân";
+            // 
+            // label12
+            // 
+            label12.AutoSize = true;
+            label12.Font = new Font("Times New Roman", 14.1F);
+            label12.Location = new Point(28, 166);
+            label12.Name = "label12";
+            label12.Size = new Size(466, 53);
+            label12.TabIndex = 0;
+            label12.Text = "Thuế Thu nhập cá nhân";
+            // 
+            // MidTermTest
+            // 
+            AutoScaleDimensions = new SizeF(17F, 41F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(2147, 1667);
+            Controls.Add(textBox8);
+            Controls.Add(comboBox1);
+            Controls.Add(label9);
+            Controls.Add(textBox1);
+            Controls.Add(textBox6);
+            Controls.Add(textBox7);
+            Controls.Add(label2);
+            Controls.Add(label6);
+            Controls.Add(label8);
+            Controls.Add(label7);
+            Controls.Add(textBox5);
+            Controls.Add(button4);
+            Controls.Add(button3);
+            Controls.Add(button2);
+            Controls.Add(button1);
+            Controls.Add(groupBox1);
+            Controls.Add(groupBox2);
+            Controls.Add(label1);
+            Name = "MidTermTest";
+            Text = "220284312 Lê Huy Lâm";
+            groupBox2.ResumeLayout(false);
+            groupBox2.PerformLayout();
+            groupBox1.ResumeLayout(false);
+            groupBox1.PerformLayout();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private TextBox textBox6;
+        private TextBox textBox7;
+        private Label label6;
+        private Label label7;
+        private TextBox textBox5;
+        private Button button4;
+        private Button button3;
+        private Button button2;
+        private Button button1;
+        private Label label2;
+        private TextBox textBox2;
+        private TextBox textBox1;
+        private Label label3;
+        private GroupBox groupBox2;
+        private Label label1;
+        private Label label8;
+        private ComboBox comboBox1;
+        private TextBox textBox3;
+        private Label label4;
+        private Label label9;
+        private TextBox textBox8;
+        private TextBox textBox4;
+        private Label label5;
+        private GroupBox groupBox1;
+        private TextBox textBox9;
+        private TextBox textBox10;
+        private TextBox textBox11;
+        private Label label10;
+        private Label label11;
+        private Label label12;
+    }
+}
